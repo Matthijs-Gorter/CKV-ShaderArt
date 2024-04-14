9a06bfc8a504373f0cef96501d877fe0ec5c752a
matthijs <72670748+Matthijs-Gorter@users.noreply.github.com>
2024-04-14 14:12:01 +0200

    added image zoom

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ window.addEventListener('scroll', function () {
     parallaxImage.style.transform = 'translateY(' + scrollPosition * -1.5 + 'px)';
     parallaxHeader.style.transform = 'translateY(' + scrollPosition * -1.5 + 'px)';
 });
+
+var images = document.querySelectorAll('.expandable-img');
+var expandedImage = document.getElementById('expandedImage');
+var overlay = document.getElementById('overlay');
+
+// Loop through each image and add a click event listener
+images.forEach(function (image) {
+    image.addEventListener('click', function () {
+        // When any image is clicked, print its src
+        console.log('Image source:', this.src);
+        expandedImage.src = this.src
+        overlay.classList.add("open")
+    });
+});
